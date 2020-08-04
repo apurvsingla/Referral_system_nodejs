1376168d3eca4cac51eac084ebb87c1edba1881a
Apurv <59192695+apurvsingla@users.noreply.github.com>
2020-08-04 11:43:17 +0530

    Update authentication.js
    return statements added

--- Controllers/authentication.js
+++ Controllers/authentication.js
@@ -9,7 +9,7 @@ module.exports.create = function(req, res) {
     User.findOne({ email: req.body.email }, function(err, user) {
         if (err) {
             console.log('error! please check the details');
-            return;
+            return res.redirect('back');
         }
         if (!user) {
             User.create(req.body, function(err, user) {
@@ -37,7 +37,7 @@ module.exports.createSession = async function(req, res) {
 
     } catch (err) {
         console.log('********', err);
-        return;
+        return res.redirect('back');
     }
 };
 
